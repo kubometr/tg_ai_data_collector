8b864a54d8eb9586a909e63dad8142c6c64d9c2f
kubometr <kubometr@email.cz>
2025-06-08 20:57:32 +0200

    Delete bot/config.py

--- bot/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from parsers.parser_manager import ParserManager
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-
-PARSER_MANAGER = ParserManager()
