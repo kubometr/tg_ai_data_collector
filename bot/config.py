7f28998fb310d5ebf79a3ca19b8e62ee5fbcad47
kubometr <kubometr@email.cz>
2025-06-08 20:30:16 +0200

    config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,8 @@
 import os
+from parsers.parser_manager import ParserManager
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
+
+PARSER_MANAGER = ParserManager()
